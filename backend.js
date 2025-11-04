bf06d758ca769a826b0e53191087388f7d2de7bd
Diego Piol Amancio <25.00475-5@maua.br>
2025-11-04 10:14:53 -0300

    feat: token para o login

--- backend.js
+++ backend.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const uniqueValidator = require("mongoose-unique-validator")
 require("dotenv").config();
 app.use(express.json());
@@ -11,14 +12,14 @@ const mongoose = require("mongoose");
 
 
 const Filme = mongoose.model("Filme", mongoose.Schema({
-    titulo: {type: String},
-    sinopse: {type: String}
+    titulo: { type: String },
+    sinopse: { type: String }
 }))
 
 
 const usuarioSchema = mongoose.Schema({
-    login: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    login: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 })
 usuarioSchema.plugin(uniqueValidator)
 const Usuario = mongoose.model("Usuario", usuarioSchema)
@@ -29,7 +30,7 @@ const stringConexao = process.env.CONEXAO_BD;
 
 async function conectarAoMongoDB() {
     await mongoose.connect(stringConexao);
-}   
+}
 
 // Requisição GET no endereço http://localhost:3000/Ola
 app.get("/Ola", (req, res) => {
@@ -46,23 +47,23 @@ let filmes = [
     // titulo: "Um Sonho de Liberdade",
     // sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
     // },
-    ];
+];
 
 
 // Requisição GET para obter a lista de filmes: http://localhost:3000/filmes
-app.get("/filmes", async(req, res) => {
+app.get("/filmes", async (req, res) => {
     const filmes = await Filme.find()
     res.json(filmes);
 })
 
 // Requisição para cadastrar um novo filme no banco
 // post http://localhost:3000/filmes
-app.post("/filmes", async(req, res) => {
+app.post("/filmes", async (req, res) => {
     // Obtem as informações que chegam
     const titulo = req.body.titulo;
     const sinopse = req.body.sinopse;
     // Monta um objeto de acordo com o model Filme
-    const filme = new Filme( {titulo: titulo, sinopse: sinopse});
+    const filme = new Filme({ titulo: titulo, sinopse: sinopse });
     // mandar o filme para o banco
     await filme.save()
     // Buscar a lista de filmes atualizada do banco
@@ -70,11 +71,12 @@ app.post("/filmes", async(req, res) => {
     res.json(filmes);
 })
 
-app.post('/signup', async(req, res) => {
-    try {const login = req.body.login
+app.post('/signup', async (req, res) => {
+    try {
+        const login = req.body.login
         const password = req.body.password
         const passwordCriptografada = await bcrypt.hash(password, 10)
-        const usuario = new Usuario({ login: login, password: passwordCriptografada})
+        const usuario = new Usuario({ login: login, password: passwordCriptografada })
         const respMongo = await usuario.save()
         console.log(respMongo)
         res.status(201).end()
@@ -86,24 +88,29 @@ app.post('/signup', async(req, res) => {
 })
 
 
-app.post('/login', async (req, res) => {
-    // capturar oque o usuário digitou
-    const login = req.body.login
-    const password = req.body.password
-    // faz a busca no MongoDB
-    const user = await Usuario.findOne({login: login})
+app.post("/login", async (req, res) => {
+    // Capturar oque o usuário digitou
+    const login = req.body.login;
+    const password = req.body.password;
+    // Faz a busca no Mongo
+    const user = await Usuario.findOne({ login: login });
     if (!user) {
         // Usuário não foi encontrado
-        return response.status(401).json({mensagem: "usuário inválido"})
+        return res.status(401).json({ mensagem: "Usuário inválido" });
     }
-    const senhaValida = bcrypt.compare(password, usser.password)
-if (!senhaValida) {
-    //  senha incorrea
-    return res.status(401).json({mensagem: "senha inválida"})
+    const senhaValida = await bcrypt.compare(password, user.password);
+    if (!senhaValida) {
+        // Senha incorreta
+        return res.status(401).json({ mensagem: "Senha inválida" });
     }
-
-    // gerar o 
-})
+    // Gerar o token (jwt)
+    const token = jwt.sign(
+        {login: login}, 
+        "chave-secreta",
+        {expiresIn: "1h"}
+    )
+    res.status(200).json({token: token});
+});
 
 app.listen(3000, () => {
     try {
